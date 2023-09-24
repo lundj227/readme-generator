b6a699033b50f058b68892d45fdda94996ddf956
Jack Lund <jacklund@Jacks-MacBook-Pro.local>
2023-09-24 16:34:44 -0700

    Added the questions and markdown for the readme file being generated

--- index.js
+++ index.js
@@ -1,12 +1,90 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What should the title of your README be?',
+    'Provide a short description explaining the what, why, and how of your project.',
+    'Is your README long enough for a table of contents?',
+    'How is your project installed?',
+    'How should it be used?',
+    'What licence did you use?',
+    'How can others contribute?',
+    'How should it be tested?',
+    'What is your github username?',
+    'What is an email people can use to ask you questions about your project?'];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+function writeToFile(fileName, data) {
+    inquirer
+        .prompt([
+        {
+            type: 'input',
+            message: questions[0],
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: questions[1],
+            name: 'projectDescription',
+        },
+        {
+            type: 'confirm',
+            message: questions[2],
+            name: 'tableOfContents'
+        },
+        {
+            type: 'input',
+            message: questions[3],
+            name: 'installation'
+        },
+        {
+            type: 'input',
+            message: questions[4],
+            name: 'usage'
+        },
+        {
+            type: 'list',
+            message: questions[5],
+            choices: [
+                'Apache License 2.0', new inquirer.Separator(),
+                'GNU General Public License v3.0', new inquirer.Separator(),
+                'MIT License', new inquirer.Separator(),
+                'BSD 2-Clause "Simplified" License'
+             ],
+            name: 'license'
+        },
+        {
+            type: 'input',
+            messsage: questions[6],
+            name: 'contribution'
+        },
+        {
+            type: 'How should it be tested?',
+            message: questions[7],
+            name: 'testing'
+        },
+        {
+            type: 'input',
+            message: questions[8],
+            name: 'github'
+        },
+        {
+            type: 'input',
+            message: questions[9],
+            name: 'email'
+        }
+        ])
+        .then((response) => 
+        fs.appendFile('professionalREADME.md', generateMarkdown(response), (err) => {
+            if (err) throw err;
+            console.log('Your professional README file has been created!')
+        }));
+};
+writeToFile();
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,98 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadgeAndLink(license) { //Ask about combination
+  var badge;
+  if (license === '') {
+    return ''
+  } else if (license === 'Apache License 2.0') {
+    badge = `[![License: Apache License 2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+    return badge;
+  } else if (license === 'GNU General Public License v3.0') {
+    badge = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+    return badge;
+  } else if (license === 'MIT License') {
+    badge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+    return badge;
+  } else if (license === 'BSD 2-Clause "Simplified" License') {
+    badge = `[![License: BSD 2-Clause "Simplified" License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)`;
+    return badge;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+//Took care of that in the first function
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+//First function did that
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  if (data.tableOfContents == true) {
+    return `
+# ${data.title} \n
+## Table of Contents
+[Description](link)
+[Installation](link)
+[Usage](link)
+[License](Link)
+[Contributing](link)
+[Tests](link)
+[Questions](link)
 
+### Description
+${data.projectDescription}
+
+### Installation
+${data.installation}
+
+### Usage
+${data.usage}
+
+### License
+${renderLicenseBadgeAndLink(data.license)}
+
+### Contributing
+${data.contribution}
+
+### Testing
+${data.testing}
+
+### Questions
+If you have any questions regarding any of the information above please contact me through either:\n
+Github:(${data.github})\n
+Email: (${data.email})
 `;
+  }
+  else {
+    return `
+# ${data.title} \n
+### Description
+${data.projectDescription}
+
+### Installation
+${data.installation}
+
+### Usage
+${data.usage}
+
+### License
+${renderLicenseBadgeAndLink(data.license)}
+
+### Contributing
+${data.contribution}
+
+### Testing
+${data.testing}
+
+### Questions
+If you have any questions regarding any of the information above please contact me through either:\n
+Github:(${data.github})\n
+Email: (${data.email})
+`
+  }
 }
 
 module.exports = generateMarkdown;
+
